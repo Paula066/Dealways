7ea4918c5c5873d5c685bbd34ab7a5573ac99b65
Paula066 <36409050+Paula066@users.noreply.github.com>
2020-04-13 19:07:28 +0200

    resposive icon hamubrger

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use script'
 const headbar = document.querySelector('.headbar');
 const headbarContainer = document.querySelector('.headbar > .container')
 window.addEventListener('scroll', () => {
@@ -12,5 +13,71 @@ window.addEventListener('scroll', () => {
     } else {
         headbarContainer.classList.add('full', 'shadow')
     }
-    console.log({ headbarContainer })
+
 })
+
+const btnMoreTranings = document.getElementById('trainings-button');
+const traningRow = document.querySelectorAll('.tranings__item-row.hide');
+
+
+const showMoreTranings = () => {
+    traningRow.forEach((e) => {
+        e.classList.remove('hide');
+    })
+    btnMoreTranings.style.display = "none";
+}
+
+btnMoreTranings.addEventListener('click', showMoreTranings);
+
+// const elementContact = document.querySelector('.list__text');
+const test = document.querySelector('.specialize');
+// const nav = document.querySelectorAll('.list');
+
+
+// const scrollView = (e, element) => {
+//     e.preventDefault();
+//     element.scrollIntoView({ behavior: "smooth" });
+
+// }
+
+
+
+// elementContact.addEventListener('click', function(e) {
+//     const test = document.querySelector('.specialize');
+//     scrollView(e, test);
+// });
+
+const elementContact = document.querySelectorAll('.list__text');
+
+
+
+// elementContact.forEach((e) => {
+//     e.addEventListener('click', (event) => {
+
+//         event.preventDefault();
+//         test.scrollIntoView();
+//     })
+// })
+
+const goto = function(e) {
+    e.preventDefault();
+    const href = this.getAttribute('href');
+    const handler = document.querySelector(href);
+    history.replaceState(null, document.title, href)
+    handler.scrollIntoView({ behavior: 'smooth' })
+}
+
+document.querySelectorAll('.goto').forEach(item => {
+    item.addEventListener('click', goto)
+})
+
+const burger = document.querySelector('.headbar__burger');
+const navMobile = document.querySelector('nav.mobile');
+
+const showMenu = () => {
+    console.log('klik');
+    // navMobile.style.display = "block";
+    navMobile.classList.toggle('show');
+}
+
+burger.addEventListener('click', showMenu)
